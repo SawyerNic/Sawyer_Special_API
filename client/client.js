df543202d84b97085a6d6372d40bcdb0e65919e9
SawyerNic <111994367+SawyerNic@users.noreply.github.com>
2024-02-23 14:26:44 -0500

    packages

---
+++ client/client.js
@@ -0,0 +1,124 @@
+//Handles our FETCH response. This function is async because it
+//contains an await.
+const handleResponse = async (response, parseResponse) => {
+
+    //Grab the content section
+    const content = document.querySelector('#content');
+
+    //Based on the status code, display something
+    switch (response.status) {
+        case 200: //success
+            content.innerHTML = `<b>Success</b>`;
+            break;
+        case 201: //created
+            content.innerHTML = '<b>Created</b>';
+            break;
+        case 204: //updated (no response back from server)
+            content.innerHTML = '<b>Updated (No Content)</b>';
+            return;
+        case 400: //bad request
+            content.innerHTML = `<b>Bad Request</b>`;
+            break;
+        case 404:
+            content.innerHTML = `<b>Not Found</b>`;
+            break;
+        default: //any other status code
+            content.innerHTML = `Error code not implemented by client.`;
+            break;
+    }
+
+    //Parse the response to json. This works because we know the server always
+    //sends back json. Await because .json() is an async function.
+    if (parseResponse) {
+        let obj = await response.json();
+
+        if (obj.message) {
+            console.log(obj);
+            content.innerHTML += `<p>Message: ${obj.message}</p>`;
+        }
+
+        if (obj.userList) {
+            // Object.values(obj.userList).map(user => {
+            //     content.innerHTML += `<p>{"${user.name}": {"name":"${user.name}","age":"${user.age}"}}</p>`;
+            // })
+            content.innerHTML += `<p>${JSON.stringify(obj.userList)}</p>`;
+        }
+    }
+
+
+};
+
+const sendGet = async (userForm) => {
+    const action = document.querySelector('#urlField').value;
+    const nameMethod = document.querySelector('#methodSelect').value.toUpperCase();
+
+    let response = await fetch(action, {
+        method: nameMethod,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        }
+    })
+
+    handleResponse(response, nameMethod === 'GET');
+
+}
+
+//Uses fetch to send a postRequest. Marksed as async because we use await
+//within it.
+const sendPost = async (nameForm) => {
+    //Grab all the info from the form
+    const nameAction = nameForm.getAttribute('action');
+    const nameMethod = nameForm.getAttribute('method');
+
+    const nameField = nameForm.querySelector('#nameField');
+    const ageField = nameForm.querySelector('#ageField');
+
+    //Build a data string in the FORM-URLENCODED format.
+    const formData = `name=${nameField.value}&age=${ageField.value}`;
+
+    //Make a fetch request and await a response. Set the method to
+    //the one provided by the form (POST). Set the headers. Content-Type
+    //is the type of data we are sending. Accept is the data we would like
+    //in response. Then add our FORM-URLENCODED string as the body of the request.
+    let response = await fetch(nameAction, {
+        method: nameMethod,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+        },
+        body: formData,
+    });
+
+    //Once we have a response, handle it.
+    handleResponse(response);
+};
+
+//Init function is called when window.onload runs (set below).
+const init = () => {
+    //Grab the form
+    const nameForm = document.querySelector('#nameForm');
+    const userForm = document.querySelector('#userForm');
+
+    //Create an addUser function that cancels the forms default action and
+    //calls our sendPost function above.
+    const addUser = (e) => {
+        e.preventDefault();
+        sendPost(nameForm);
+        return false;
+    }
+
+    const getUser = (e) => {
+        e.preventDefault();
+        sendGet(userForm);
+        return false;
+    }
+
+    //Call addUser when the submit event fires on the form.
+    nameForm.addEventListener('submit', addUser);
+    userForm.addEventListener('submit', getUser);
+
+};
+
+//When the window loads, run init.
+window.onload = init;
---
+++ src/server.js
@@ -0,0 +1,77 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUser,
+    '/bundle.js': htmlHandler.getBundle,
+    '/success': jsonHandler.success,
+    '/badRequest': jsonHandler.badRequest,
+    '/notReal': jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUser,
+    '/notReal': jsonHandler.notFound,
+  },
+  notFound: jsonHandler.notFound,
+};
+
+const parseBody = (request, response, handler) => {
+  const body = [];
+
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    const bodyParams = query.parse(bodyString);
+
+    handler(request, response, bodyParams);
+  });
+};
+
+// handle POST requests
+const handlePost = (request, response, parsedUrl) => {
+  // If they go to /addUser
+  if (parsedUrl.pathname === '/addUser') {
+    // Call our below parseBody handler, and in turn pass in the
+    // jsonHandler.addUser function as the handler callback function.
+    parseBody(request, response, jsonHandler.addUser);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if (request.method === 'GET') {
+    // GET method
+    urlStruct.GET[parsedUrl.pathname](request, response, params);
+  } else if (request.method === 'HEAD') {
+    // HEAD method
+    urlStruct.HEAD[parsedUrl.pathname](request, response, params);
+  } else if (request.method === 'POST') {
+    // POST method
+    handlePost(request, response, parsedUrl);
+  } else {
+    // not found
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+});
